653fece6a915f986007381ffe9581419f625a612
Zirui Ding <zirui.ding@kcl.ac.uk>
2022-02-01 18:13:47 +0000

    create home page

--- bookclubs/views.py
+++ bookclubs/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
+#from bookclubs.helpers import login_prohibited
+from django.contrib.auth.hashers import check_password
+from django.urls import reverse
 
-# Create your views here.
+#@login_prohibited
+def home(request):
+    return render(request, 'home.html')
