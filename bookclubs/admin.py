f0235e9b3c1d4aeea4af7b0d335c6e6f2bfd54da
ace1220 <86724651+ace1220@users.noreply.github.com>
2022-02-07 01:39:14 +0000

    Registered models in admin.py.

--- bookclubs/admin.py
+++ bookclubs/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import User, Application, Club
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username',
+                    'email']
+
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ['id',
+                    'club_name']
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'club',
+                    'statement']
