5d0bc73388b4a6e236e2d46f7944a661f85b76a9
Encheng Wu <vincewu7@gmail.com>
2022-02-12 04:30:50 +0000

    fix rating data error but need fix others

--- bookclubs/recommender/BooksRecommender.py
+++ bookclubs/recommender/BooksRecommender.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from posixpath import sep
 import sys
 import re
 import urllib.request
@@ -7,6 +8,7 @@ import json
 import textwrap
 import zipfile
 import codecs
+import pandas as pd
 # Part of getGenra code comes from https://gist.github.com/AO8/faa3f52d3d5eac63820cfa7ec2b24aa7
 from surprise import Dataset
 from surprise import Reader
@@ -16,42 +18,71 @@ from collections import defaultdict
     
 class BooksRecommender:
 
-    def loadData():
-        user_book_rating = zipfile.ZipFile('./dataset/book-review-dataset.zip')
-        user_book_rating.extractall("./dataset") 
-        user_book_rating.close()
-        # unzip the file cuz its to large to put in
-        # load user information, get userID and other info
-        users = {}
-        with codecs.open('./dataset/BX-Users.csv', 'r', 'utf-8') as user:
-            for line in user:
-                features = list(map(lambda x: x.strip('"'), line.split(';')))
-                users[features[0]] = "--".join(features[1:])
+    # def loadData(self):
+    #     user_book_rating = zipfile.ZipFile('./dataset/book-review-dataset.zip')
+    #     user_book_rating.extractall("./dataset") 
+    #     user_book_rating.close()
+    #     # unzip the file cuz its to large to put in
+    #     # load user information, get userID and other info
+    #     users = {}
+    #     with codecs.open('./dataset/BX-Users.csv', 'r', 'latin-1') as user:
+    #         next(user)
+    #         for line in user:
+    #             features = list(map(lambda x: x.strip('"'), line.split(';')))
+    #             users[features[0]] = "--".join(features[1:])
 
-    #     books = pd.read_csv('./dataset/BX-Books.csv', sep=';')
-    #     user = pd.read_csv('./dataset/BX-Users.csv', sep=';')
-    #     ratings = pd.read_csv('./dataset/BX-Books-Ratings.csv', sep=';')
-        
-        return users
+    #     books = {}
+    #     with codecs.open('./dataset/BX-Books.csv', 'r', 'latin-1') as book:
+    #         next(book)
+    #         for line in book:
+    #             features = list(map(lambda x: x.strip('"'), line.split(';')))
+    #             books[features[0]] = '--'.join(features[1:])
+
+    #     ratings = {}
+    #     with codecs.open('./dataset/BX-Book-Ratings.csv', 'r', 'latin-1') as rating:
+    #         next(rating)
+    #         for line in rating:
+    #             features = list(map(lambda x: x.strip('"'), line.split(';')))
+    #             if features[0] in ratings:
+    #                 current_ratings = ratings[features[0]]
+    #             else:
+    #                 current_ratings = {}
+    #             current_ratings[features[1]] = int(features[2].strip().strip('"'))
+    #             ratings[features[0]] = current_ratings
+
+    # #     books = pd.read_csv('./dataset/BX-Books.csv', sep=';')
+    # #     user = pd.read_csv('./dataset/BX-Users.csv', sep=';')
+    # #     ratings = pd.read_csv('./dataset/BX-Books-Ratings.csv', sep=';')
+    #     df = pd.DataFrame({ 'User-ID': users,
+    #                         'ISBN'   : books,
+    #                         'rating' : ratings})
+    #     return df
+
+
+
+    os.chdir(os.path.dirname(sys.argv[0]))
+
     
     isbn_to_bookTitle = {}
     bookTitle_to_isbn = {}
     ratingsPath = './dataset/BX-Book-Ratings.csv'
     booksPath   = './dataset/BX-Books.csv'
     
+    def loadRatingData(self):
+        df = pd.read_csv(self.ratingsPath, sep = ';',names = ['User-ID', 'ISBN', 'Book-Rating'], quotechar = '"', encoding = 'latin-1',header = 0 )
+        return df
 
     def loadBookRecommenderLatestSmall(self):
 
         # Look for files relative to the directory we are running from
         os.chdir(os.path.dirname(sys.argv[0]))
-
         ratingsDataset = 0
         self.isbn_to_bookTitle = {}
         self.bookTitle_to_isbn = {}
 
         reader = Reader(line_format = 'user item rating', sep = ';', skip_lines = 1, rating_scale=(1,10))
 
-        ratingsDataset = Dataset.load_from_file(self.ratingsPath, reader = reader)
+        ratingsDataset = Dataset.load_from_df(self.loadRatingData(), reader = reader)
 
         with open(self.booksPath, newline='', encoding='ISO-8859-1') as csvfile:
                 bookReader = csv.reader(csvfile)
@@ -73,7 +104,7 @@ class BooksRecommender:
                 userID = int(row[0])
                 if (user == userID):
                     isbn = int(row[1])
-                    rating = float(row[2])
+                    rating = float(row[2].strip('"'))
                     userRatings.append((isbn, rating))
                     hitUser = True
                 if (hitUser and (user != userID)):
@@ -89,7 +120,7 @@ class BooksRecommender:
                 isbn = int(row[1])
                 ratings[isbn] += 1
         rank = 1
-        for isbn, ratingCount in sorted(ratings.items(), key = lambda x: x[1], reverse = True):
+        for isbn, ratingCount in sorted(ratings.items().strip('"'), key = lambda x: x[1], reverse = True):
             rankings[isbn] = rank
             rank += 1
 
