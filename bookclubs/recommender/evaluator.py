47d3472eb7f85357f35de237d144ae672a76f7b7
Encheng Wu <vincewu7@gmail.com>
2022-02-12 00:17:05 +0000

    add evaluation methods but still a error during loading the data

--- bookclubs/recommender/evaluator.py
+++ bookclubs/recommender/evaluator.py
@@ -1,68 +1,73 @@
-from surprise.model_selection import train_test_split
-from surprise.model_selection import LeaveOneOut
-from surprise import KNNBaseline
+from EvaluationData import EvaluationData
+from EvaluatedAlgorithm import EvaluatedAlgorithm
 
-class EvaluationData:
+class Evaluator:
     
-    def __init__(self, data, popularityRankings):
-        
-        self.rankings = popularityRankings
-        
-        #Build a full training set for evaluating overall properties
-        self.fullTrainSet = data.build_full_trainset()
-        self.fullAntiTestSet = self.fullTrainSet.build_anti_testset()
-        
-        #Build a 75/25 train/test split for measuring accuracy
-        self.trainSet, self.testSet = train_test_split(data, test_size=.25, random_state=1)
-        
-        #Build a "leave one out" train/test split for evaluating top-N recommenders
-        #And build an anti-test-set for building predictions
-        LOOCV = LeaveOneOut(n_splits=1, random_state=1)
-        for train, test in LOOCV.split(data):
-            self.LOOCVTrain = train
-            self.LOOCVTest = test
-            
-        self.LOOCVAntiTestSet = self.LOOCVTrain.build_anti_testset()
-        
-        #Compute similarty matrix between items so we can measure diversity
-        sim_options = {'name': 'cosine', 'user_based': False}
-        self.simsAlgo = KNNBaseline(sim_options=sim_options)
-        self.simsAlgo.fit(self.fullTrainSet)
-            
-    def GetFullTrainSet(self):
-        return self.fullTrainSet
+    algorithms = []
     
-    def GetFullAntiTestSet(self):
-        return self.fullAntiTestSet
-    
-    def GetAntiTestSetForUser(self, testSubject):
-        trainset = self.fullTrainSet
-        fill = trainset.global_mean
-        anti_testset = []
-        u = trainset.to_inner_uid(str(testSubject))
-        user_items = set([j for (j, _) in trainset.ur[u]])
-        anti_testset += [(trainset.to_raw_uid(u), trainset.to_raw_iid(i), fill) for
-                                 i in trainset.all_items() if
-                                 i not in user_items]
-        return anti_testset
+    def __init__(self, dataset, rankings):
+        ed = EvaluationData(dataset, rankings)
+        self.dataset = ed
+        
+    def AddAlgorithm(self, algorithm, name):
+        alg = EvaluatedAlgorithm(algorithm, name)
+        self.algorithms.append(alg)
+        
+    def Evaluate(self, doTopN):
+        results = {}
+        for algorithm in self.algorithms:
+            print("Evaluating ", algorithm.GetName(), "...")
+            results[algorithm.GetName()] = algorithm.Evaluate(self.dataset, doTopN)
 
-    def GetTrainSet(self):
-        return self.trainSet
-    
-    def GetTestSet(self):
-        return self.testSet
-    
-    def GetLOOCVTrainSet(self):
-        return self.LOOCVTrain
-    
-    def GetLOOCVTestSet(self):
-        return self.LOOCVTest
-    
-    def GetLOOCVAntiTestSet(self):
-        return self.LOOCVAntiTestSet
-    
-    def GetSimilarities(self):
-        return self.simsAlgo
-    
-    def GetPopularityRankings(self):
-        return self.rankings
+        # Print results
+        print("\n")
+        
+        if (doTopN):
+            print("{:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(
+                    "Algorithm", "RMSE", "MAE", "HR", "cHR", "ARHR", "Coverage", "Diversity", "Novelty"))
+            for (name, metrics) in results.items():
+                print("{:<10} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f}".format(
+                        name, metrics["RMSE"], metrics["MAE"], metrics["HR"], metrics["cHR"], metrics["ARHR"],
+                                      metrics["Coverage"], metrics["Diversity"], metrics["Novelty"]))
+        else:
+            print("{:<10} {:<10} {:<10}".format("Algorithm", "RMSE", "MAE"))
+            for (name, metrics) in results.items():
+                print("{:<10} {:<10.4f} {:<10.4f}".format(name, metrics["RMSE"], metrics["MAE"]))
+                
+        print("\nLegend:\n")
+        print("RMSE:      Root Mean Squared Error. Lower values mean better accuracy.")
+        print("MAE:       Mean Absolute Error. Lower values mean better accuracy.")
+        if (doTopN):
+            print("HR:        Hit Rate; how often we are able to recommend a left-out rating. Higher is better.")
+            print("cHR:       Cumulative Hit Rate; hit rate, confined to ratings above a certain threshold. Higher is better.")
+            print("ARHR:      Average Reciprocal Hit Rank - Hit rate that takes the ranking into account. Higher is better." )
+            print("Coverage:  Ratio of users for whom recommendations above a certain threshold exist. Higher is better.")
+            print("Diversity: 1-S, where S is the average similarity score between every possible pair of recommendations")
+            print("           for a given user. Higher means more diverse.")
+            print("Novelty:   Average popularity rank of recommended items. Higher means more novel.")
+        
+    def SampleTopNRecs(self, ml, testSubject=85, k=10):
+        
+        for algo in self.algorithms:
+            print("\nUsing recommender ", algo.GetName())
+            
+            print("\nBuilding recommendation model...")
+            trainSet = self.dataset.GetFullTrainSet()
+            algo.GetAlgorithm().fit(trainSet)
+            
+            print("Computing recommendations...")
+            testSet = self.dataset.GetAntiTestSetForUser(testSubject)
+        
+            predictions = algo.GetAlgorithm().test(testSet)
+            
+            recommendations = []
+            
+            print ("\nWe recommend:")
+            for userID, isbn, actualRating, estimatedRating, _ in predictions:
+                intISBN = int(isbn)
+                recommendations.append((intISBN, estimatedRating))
+            
+            recommendations.sort(key=lambda x: x[1], reverse=True)
+            
+            for ratings in recommendations[:10]:
+                print(ml.getMovieName(ratings[0]), ratings[1])
