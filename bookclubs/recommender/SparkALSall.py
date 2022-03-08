94fe3bf682d7cd8299e74d06eeb16aa030ab46c9
Encheng Wu <vincewu7@gmail.com>
2022-03-08 15:04:57 +0000

    parameter fix

--- bookclubs/recommender/SparkALSall.py
+++ bookclubs/recommender/SparkALSall.py
@@ -9,7 +9,6 @@ from pyspark.sql.functions import explode
 from BookLens import BookLens
 
 import pandas as pd
-import csv
 
 from BookLens import BookLens
 
@@ -23,15 +22,19 @@ if __name__ == "__main__":
 
     spark.sparkContext.setCheckpointDir("/tmp/checkpoints")
 
+
+
     df = pd.read_csv('bookclubs/dataset/BX-Book-Ratings.csv', sep = ';',names = ['User-ID', 'ISBN', 'Book-Rating'], quotechar = '"', encoding = 'latin-1',header = 0 )
     df = df[df.loc[:]!=0].dropna()
     df.ISBN = df.ISBN.apply(lambda x: x[:-1] + "10" if x[-1] == "X" else x)
-    df = df[df['ISBN'].map(lambda x: len(x) <= 11 & len(x) >= 9)]
+    #df = df[df['ISBN'].map(lambda x: len(x) <= 11 & len(x) >= 9)]
     df = spark.createDataFrame(df)
 
     indexer = StringIndexer(inputCol="ISBN", outputCol="bookID").fit(df)
     indexed = indexer.transform(df)
     ratings = indexed.drop("ISBN")
+    ratings.show()
+
     """ lines = indexed.rdd
     ratingsRDD = lines.map(lambda p: Row(userId=int(p[0]), bookID=int(p[3]),
                                          rating=float(p[2])))
@@ -39,8 +42,9 @@ if __name__ == "__main__":
 
     (training, test) = ratings.randomSplit([0.8, 0.2])
 
-    als = ALS(maxIter=30, regParam=0.3,rank=40,userCol="User-ID", itemCol="bookID", ratingCol="Book-Rating",
+    als = ALS(maxIter=30, regParam=0.4,rank=40,userCol="User-ID", itemCol="bookID", ratingCol="Book-Rating",
               coldStartStrategy="drop")
+
     model = als.fit(training)
     predictions_test = model.transform(test)
     predictions_train = model.transform(training)
@@ -52,7 +56,6 @@ if __name__ == "__main__":
     rmse_train = evaluator.evaluate(predictions_train)
     print("Root-mean-square error for test = " + str(rmse_test))
     print("Root-mean-square error for train = " + str(rmse_train))
-
 
     userRecs = model.recommendForAllUsers(10)
 
