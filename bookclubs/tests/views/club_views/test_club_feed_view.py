b8bbf61207fae25c5d124eaf79016e2f2e16d2bc
xuechao Yan <K19006213@kcl.ac.uk>
2022-03-23 01:28:10 +0000

    add test for club feed

--- bookclubs/tests/views/club_views/test_club_feed_view.py
+++ bookclubs/tests/views/club_views/test_club_feed_view.py
@@ -21,6 +21,9 @@ class ClubFeedViewTestCase(TestCase):
         self.club4 =  Club.objects.get(club_name='public_in-person')
         self._create_test_rols()
         self.url1 = reverse('club_feed',kwargs={'club_name':self.club1.club_name})
+        self.url2 = reverse('club_feed',kwargs={'club_name':self.club2.club_name})
+        self.url3 = reverse('club_feed',kwargs={'club_name':self.club3.club_name})
+        self.url4 = reverse('club_feed',kwargs={'club_name':self.club4.club_name})
 
     def test_create_club_url(self):
         self.assertEqual(reverse('club_feed',kwargs={'club_name':self.club1.club_name}), f'/club/{self.club1.club_name}/feed/')
@@ -38,6 +41,36 @@ class ClubFeedViewTestCase(TestCase):
         self.assertEqual(len(response.context['members']), 0)
         self.assertEqual(len(response.context['management']), 1)
 
+    def test_get_club2_feed(self):
+        response = self.client.get(self.url2)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club_feed.html')
+        self.assertEqual(response.context['club'],self.club2)
+        self.assertTrue(response.context['is_moderator']==False)
+        self.assertEqual(response.context['is_owner'],False)
+        self.assertEqual(len(response.context['members']), 1)
+        self.assertEqual(len(response.context['management']), 0)
+
+    def test_get_club3_feed(self):
+        response = self.client.get(self.url3)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club_feed.html')
+        self.assertEqual(response.context['club'],self.club3)
+        self.assertTrue(response.context['is_moderator']==True)
+        self.assertEqual(response.context['is_owner'],False)
+        self.assertEqual(len(response.context['members']), 0)
+        self.assertEqual(len(response.context['management']), 1)
+
+    def test_get_club4_feed(self):
+        response = self.client.get(self.url4)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club_feed.html')
+        self.assertEqual(response.context['club'],self.club4)
+        self.assertTrue(response.context['is_moderator']==False)
+        self.assertEqual(response.context['is_owner'],True)
+        self.assertEqual(len(response.context['members']), 0)
+        self.assertEqual(len(response.context['management']), 1)
+
     def _create_test_rols(self):
         Role.objects.create(
             club = self.club1,
@@ -48,13 +81,13 @@ class ClubFeedViewTestCase(TestCase):
         Role.objects.create(
             club = self.club2,
             user = self.user,
-            club_role = 'OWN',
+            club_role = 'MEM',
             )
 
         Role.objects.create(
             club = self.club3,
             user = self.user,
-            club_role = 'OWN',
+            club_role = 'MOD',
             )
 
         Role.objects.create(
