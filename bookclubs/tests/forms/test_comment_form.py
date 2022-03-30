6583df67925bfdafe2d5ccad8ea7c0676f0a10c7
ace1220 <86724651+ace1220@users.noreply.github.com>
2022-03-30 21:57:01 +0100

    Reformatted and added comments for all form tests.

--- bookclubs/tests/forms/test_comment_form.py
+++ bookclubs/tests/forms/test_comment_form.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
-from bookclubs.models import User, Post, Comment
+
 from bookclubs.forms import CommentForm
+from bookclubs.models import User
+
 
 class CommentFormTestCase(TestCase):
+    """Unit tests for the CommentForm"""
 
-    fixtures = ['bookclubs/tests/fixtures/default_user.json']
+    fixtures = [
+        'bookclubs/tests/fixtures/default_user.json',
+    ]
 
     def setUp(self):
         super(TestCase, self).setUp()
@@ -15,11 +20,11 @@ class CommentFormTestCase(TestCase):
         self.assertIn('body', form.fields)
 
     def test_valid_comment_form(self):
-        input = {'body': 'x'*520 }
-        form = CommentForm(data=input)
+        form_input = {'body': 'x' * 520}
+        form = CommentForm(data=form_input)
         self.assertTrue(form.is_valid())
 
     def test_invalid_comment_form(self):
-        input = {'body': 'x'*521 }
-        form = CommentForm(data=input)
+        form_input = {'body': 'x' * 521}
+        form = CommentForm(data=form_input)
         self.assertFalse(form.is_valid())
